69a78f22522adbe69a73553c7a6fa9d6e854b998
shubhamdogra418 <shubhamdogra418@gmail.com>
2024-01-11 20:07:48 +0530

    stack using Linked List

--- Stack/implementUsingLL.js
+++ Stack/implementUsingLL.js
@@ -1,12 +1,47 @@
 class Node {
     constructor(data) {
-        this.data= data;
-        this.next=null;
+        this.data = data;
+        this.next = null;
     }
 }
+
 class StackLinkedList {
     constructor() {
-        this.top=null;
-        this.size=0;
+        this.top = null;
+        this.size = 0;
+    }
+
+    push(data) {
+        const newNode = new Node(data);
+        newNode.next = this.top;
+        this.top = newNode;
+        this.size++;
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            return "List is already empty"
+        }
+        const item = this.top.data;
+        this.top = this.top.next;
+        this.size--;
+        return item;
+    }
+
+    peek(){
+        return this.top.data;
+    }
+
+    isEmpty() {
+        return this.size === 0;
     }
 }
+
+const stack = new StackLinkedList()
+stack.push(10)
+stack.push(20)
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.isEmpty());
+
+console.log(stack)
