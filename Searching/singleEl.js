e8d15836d722ea5fa454b7326fa67d12490f044a
shubhamdogra418 <shubhamdogra418@gmail.com>
2024-01-14 01:59:02 +0530

    540. Single Element M

--- Searching/singleEl.js
+++ Searching/singleEl.js
@@ -30,24 +30,20 @@ function singleEL(nums) {
 function singleElBS(nums) {
     let low=0;
     let high=nums.length-1;
-    if(!nums.length) return -1;
-    if(nums[0] !== nums[1]) {
-        return nums[0];
-    }
-    if(nums[nums.length-1] !== nums[nums.length-2]) {
-        return nums[nums.length-1];
-    }
     while(low<=high) {
         let mid=Math.floor((low+high)/2);
         if(nums[mid] !== nums[mid-1] && nums[mid] !== nums[mid+1]) {
             return nums[mid];
-        } else if(nums[mid] !== nums[mid-1] && nums[mid] === nums[mid+1]) {
+        } if ((mid%2 ===0 && nums[mid+1] === nums[mid]) || 
+            (mid%2 !==0 && nums[mid+1] !== nums[mid] )) {
+            low=mid+1;
+        } else {
             high=mid-1;
-        } else low=mid+1;
+        }
     } return -1;
 }
-let nums = [2,2,7,7,10,10,11,11,12];
+let nums = [3,3,7,7,10,11,11];
 // console.log(singleEL(nums));
-// console.log(singleElBS(nums));
+console.log(singleElBS(nums));
 console.log(xoruse(nums));
 console.log(looping(nums));
